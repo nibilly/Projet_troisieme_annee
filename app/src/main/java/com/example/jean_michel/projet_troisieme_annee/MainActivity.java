25a1a5a39dce0370a5d4c9f6f414c66c45e7896d
unknown <nicolas@LT-6TH75S2.local.isima.fr>
2020-02-04 18:40:18 +0100

    recuoeration des appareils connectes en bluetooth

--- app/src/main/java/com/example/jean_michel/projet_troisieme_annee/MainActivity.java
+++ app/src/main/java/com/example/jean_michel/projet_troisieme_annee/MainActivity.java
@@ -2,13 +2,62 @@ package com.example.jean_michel.projet_troisieme_annee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String deviceAddress = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> deviceStrs = new ArrayList<>();
+        final ArrayList<String> devices = new ArrayList<>();
+
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0)
+        {
+            for (BluetoothDevice device : pairedDevices)
+            {
+                deviceStrs.add(device.getName() + "\n" + device.getAddress());
+                devices.add(device.getAddress());
+            }
+        }
+
+        // show list
+        final AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+
+        ArrayAdapter<Object> adapter = new ArrayAdapter<>(this, android.R.layout.select_dialog_singlechoice,
+                deviceStrs.toArray(new Object[0]));
+
+        alertDialog.setSingleChoiceItems(adapter, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                dialog.dismiss();
+                int position = ((AlertDialog) dialog).getListView().getCheckedItemPosition();
+                String deviceAddressTemp = devices.get(position);
+                MainActivity.deviceAddress = deviceAddressTemp;
+                TextView textViewDeviceAddress = findViewById(R.id.textViewDeviceAddress);
+                textViewDeviceAddress.setText(deviceAddressTemp);
+            }
+        });
+
+        alertDialog.setTitle("Choose Bluetooth device already paired");
+        alertDialog.show();
+
     }
 }
+;
