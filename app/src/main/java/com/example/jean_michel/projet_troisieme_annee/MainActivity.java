46e30cf86cc15db82d66d5d3997a2d6b3f2cedd5
unknown <nicolas@LT-6TH75S2.local.isima.fr>
2020-02-10 18:58:57 +0100

    Connection OBD2 device, log engine RPM and speed

--- app/src/main/java/com/example/jean_michel/projet_troisieme_annee/MainActivity.java
+++ app/src/main/java/com/example/jean_michel/projet_troisieme_annee/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.jean_michel.projet_troisieme_annee;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.DialogInterface;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.pires.obd.commands.SpeedCommand;
+import com.github.pires.obd.commands.engine.RPMCommand;
+import com.github.pires.obd.commands.protocol.EchoOffCommand;
+import com.github.pires.obd.commands.protocol.LineFeedOffCommand;
+import com.github.pires.obd.commands.protocol.SelectProtocolCommand;
+import com.github.pires.obd.commands.protocol.TimeoutCommand;
+import com.github.pires.obd.enums.ObdProtocols;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -52,12 +65,50 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.deviceAddress = deviceAddressTemp;
                 TextView textViewDeviceAddress = findViewById(R.id.textViewDeviceAddress);
                 textViewDeviceAddress.setText(deviceAddressTemp);
+                obd2();
             }
         });
 
-        alertDialog.setTitle("Choose Bluetooth device already paired");
+        alertDialog.setTitle("Choose OBDII device already paired");
         alertDialog.show();
 
     }
+
+    private void obd2()
+    {
+        // connection
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        BluetoothDevice device = btAdapter.getRemoteDevice(deviceAddress);
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+        BluetoothSocket socket = null;
+        try {
+            socket = device.createInsecureRfcommSocketToServiceRecord(uuid);
+            socket.connect();
+
+            // config
+            new EchoOffCommand().run(socket.getInputStream(), socket.getOutputStream());
+
+            new LineFeedOffCommand().run(socket.getInputStream(), socket.getOutputStream());
+
+            new TimeoutCommand(250).run(socket.getInputStream(), socket.getOutputStream());
+
+            new SelectProtocolCommand(ObdProtocols.AUTO).run(socket.getInputStream(), socket.getOutputStream());
+
+            RPMCommand engineRpmCommand = new RPMCommand();
+            SpeedCommand speedCommand = new SpeedCommand();
+            while (!Thread.currentThread().isInterrupted())
+            {
+                engineRpmCommand.run(socket.getInputStream(), socket.getOutputStream());
+                speedCommand.run(socket.getInputStream(), socket.getOutputStream());
+                // TODO handle commands result
+                Log.d("MAin", "RPM: " + engineRpmCommand.getFormattedResult());
+                Log.d("MAin", "Speed: " + speedCommand.getFormattedResult());
+                Thread.sleep(1000);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
-;
